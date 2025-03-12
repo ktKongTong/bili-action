0b903862c1cc76e57073ef415943656b8a29826e
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2025-03-12 22:44:40 +0800

    refactor: update action name and description, change input to bvid

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+
+function getVideoByBvid(bvid: string): Promise<any> {
+  return fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`).then(res => res.json())
+}
 
 /**
  * The main function for the action.
@@ -8,18 +11,9 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const bvid: string = core.getInput('bvid')
+    await getVideoByBvid(bvid)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
